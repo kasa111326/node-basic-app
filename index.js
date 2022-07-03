ed510ced6c58f46c32a263575ef9dc09e5af1a66
Kasa Sathish <kasathis@microsoft.com>
2022-07-03 11:00:31 +0530

    added the basic app

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+
+const router = express.Router();
+const app = express()
+const port = 8000
+
+// add router in express app
+app.use("/",router);
+
+
+router.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+router.post("/hook",(req, res) => {
+    const data = req.body;
+    console.log("Subscribe Request");
+    console.log(data);
+    res.status(201);
+    res.json({id: 66, apiStatus: "success"});
+});
+
+router.delete("/hook",(req, res) => {
+    const data = req.body;
+    console.log("Unsubscribe Request");
+    console.log(data);
+    res.status(204);
+    res.json({apiStatus: "success"});
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
